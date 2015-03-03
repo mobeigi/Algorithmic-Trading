a9b5b3f1ad3c5c5c8d8217ea6afeb442c518b38e
SamuelWhitton <admin@Macintosh-3.local>
2015-03-03 12:04:49 +1100

    start

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Algorithmic Trading COMP3011
+//
+//  Created on 03/03/15.
+//  Copyright (c) Revitpo. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+namespace std {
+
+	int main(int argc, const char * argv[]) {
+		cout << "Hello Merth";
+	}
+
+}
