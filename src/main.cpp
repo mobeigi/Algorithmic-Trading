75e47ce3a0d71f27fd0415627e5880d09424ac6f
Jason Ng <jsfn660@jsfn660-macbook-air.local>
2015-03-05 15:33:28 +1100

    added revised file i/o for SMA calculation testing

--- src/main.cpp
+++ src/main.cpp
@@ -10,61 +10,64 @@
 
 #include <iostream>
 
-#include <ifstream>
+#include <fstream>
 #include <vector>
+#include <cstdlib>
 
 #include "MomentumStrategy.h"
 #include "Logger.h"
 #include "TradeDay.h"
 
 
-void test() {
+void test(std::vector<std::TradeDay> trades) {
     std::Logger logger = std::Logger::standardLogger();
-    std::vector<std::TradeDay> trades = {
-        std::TradeDay(30.0),
-        std::TradeDay(30.34),
-        std::TradeDay(30.71),
-        std::TradeDay(31.09),
-        std::TradeDay(31.29),
-        std::TradeDay(31.54),
-        std::TradeDay(31.76),
-        std::TradeDay(31.98),
-        std::TradeDay(32.58)};
+    // std::vector<std::TradeDay> trades = {
+    //     std::TradeDay(30.0),
+    //     std::TradeDay(30.34),
+    //     std::TradeDay(30.71),
+    //     std::TradeDay(31.09),
+    //     std::TradeDay(31.29),
+    //     std::TradeDay(31.54),
+    //     std::TradeDay(31.76),
+    //     std::TradeDay(31.98),
+    //     std::TradeDay(32.58)};
+
+   //  for(unsigned i = 0; i != trades.size(); i++) {
+   //        std::cout << trades.at(i).getLastPrice() << '\n';
+   // }
+
     std::StrategyResult result = std::MomentumStrategy(logger, trades, 3).execute();
     logger.log(result.description());
 }
 
 int main(int argc, const char * argv[]) {
-    test();
+    // we can either store the file into memory? or just grab the shit we need?
+    // since we need to implement different strategies later?
+    std::ifstream d;
+    std::string m;
+    std::vector<std::string> tradingData, temp;
+    std::vector<std::TradeDay> trades;
 
-   //  std::ifstream d;
-   //  std::string m, b;
-   //  std::vector<std::string> c;
-   //  std::vector<std::vector<std::string> > t;
-   //  d.open("BHP_Daily_Since2000.csv");
-   //  if(d.is_open()) {
-   //       while(getline(d, m)) {
-   //             if(m.at(0) == '#') continue;
-   //             for(unsigned i = 0; i < m.length(); ++i) {
-   //                   if(m[i] != ',' && i + 1 != m.length()) {
-   //                         b += m[i];
-   //                   } else {
-   //                         c.push_back(b);
-   //                         b = "";
-   //                   }
-   //             }
-   //             t.push_back(c);
-   //       }
-   // }
-   // d.close();
-   // for(unsigned n = 0; n != t.size(); n++) {
-   //       c = t.at(n);
-   //       for(unsigned o = 0; o != c.size(); o++) {
-   //             if(c.at(o).empty()) std::cout << "(null)" << '\n';
-   //             else std::cout << c.at(o) << '\n';
-   //       }
-   // }
+    //if(argc != 2) return false;
+    d.open("BHP_Daily_Since2000.csv");
+    if(d.is_open()) {
+          while(getline(d, m)) tradingData.push_back(m);
+          d.close();
 
-
+          // we need to figure out what data we want to take from the parsed shit
+          // i.e. should pass in the tradingData to the strategy
+          for(unsigned n = 0; n != tradingData.size(); n++) {
+                m = tradingData.at(n);
+                for(unsigned i = 0; i < m.length(); i++) {
+                      unsigned b = i;
+                      for(; m[i] != ',' && i + 1 != m.length(); i++);
+                      temp.push_back(m.substr(b, i - b));
+                }
+                //std::cout << temp.at(8) << '\n';
+                if(!temp.at(8).empty()) trades.push_back(std::TradeDay(std::atof(temp.at(8).c_str())));
+                temp.clear();
+          }
+    }
+    test(trades);
     return 0;
 }
