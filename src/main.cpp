22c22425a23c743a48eface062b2ed6f94190f23
Jason Ng <jsnng@users.noreply.github.com>
2015-03-06 12:42:12 +1100

    .

--- src/main.cpp
+++ src/main.cpp
@@ -19,19 +19,8 @@
 #include "TradeDay.h"
 
 
-// void test(std::vector<std::TradeDay> trades) {
-void test () {
+void test(std::vector<std::TradeDay> trades) {
     std::Logger logger = std::Logger::standardLogger();
-    std::vector<std::TradeDay> trades = {
-        std::TradeDay(30.0),
-        std::TradeDay(30.34),
-        std::TradeDay(30.71),
-        std::TradeDay(31.09),
-        std::TradeDay(31.29),
-        std::TradeDay(31.54),
-        std::TradeDay(31.76),
-        std::TradeDay(31.98),
-        std::TradeDay(32.58)};
     std::StrategyResult result = std::MomentumStrategy(logger, trades, 3).execute();
     logger.log(result.description());
 }
@@ -64,6 +53,6 @@ int main(int argc, const char * argv[]) {
                 temp.clear();
           }
     }
-    test();
+    test(trades);
     return 0;
 }
