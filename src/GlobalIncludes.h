cd4931da351cb20fd56cd10bda9d703c4c24596b
mohammadg <webmaster@mohammadg.com>
2015-03-24 11:17:29 +1100

    Added padding to DateTime function

--- src/GlobalIncludes.h
+++ src/GlobalIncludes.h
@@ -28,18 +28,25 @@
 namespace std {
     class Helper {
     public:
+		  //Returns a date as a string in format HH:MM:SS DD:MM:YYYY
           static string datetime() {
           time_t t = time(0);
           struct tm * now = localtime(&t);
           stringstream ss;
-          ss << (now->tm_hour) << ':';
-          ss << (now->tm_min) << ':';
-          ss << (now->tm_sec) << ' ';
-          ss << (now->tm_mday) << ':';
-          ss << (now->tm_mon + 1) << ':';
-          ss << (now->tm_year + 1900);
+		  ss << padDigit(now->tm_hour) << ':';
+		  ss << padDigit(now->tm_min) << ':';
+		  ss << padDigit(now->tm_sec) << ' ';
+		  ss << padDigit(now->tm_mday) << ':';
+		  ss << padDigit(now->tm_mon + 1) << ':';
+		  ss << now->tm_year + 1900;
           return ss.str();
-    }
+		}
+		
+		//Pad digits smaller than 10 with zeros to maintain length of components such as dates
+		static inline string padDigit(int number) {
+			return ((number < 10) ? ("0" + to_string(number)) : to_string(number));
+		}
+
         static string formatPrice(double price) {
 			stringstream ss;
 			ss << setprecision(2) << std::fixed << "$" << price;
