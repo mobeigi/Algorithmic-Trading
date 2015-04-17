430d3f1727a0d72c1280a5bf405416ee651605f8
zychreo95 <cool.ian.w@hotmail.com>
2015-04-17 15:15:28 +1000

    Added params validity checking

--- build-AlgorithmicTrading-Desktop_Qt_5_4_1_MinGW_32bit-Debug/debug/moc_mainwindow.cpp
+++ build-AlgorithmicTrading-Desktop_Qt_5_4_1_MinGW_32bit-Debug/debug/moc_mainwindow.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[5];
-    char stringdata[83];
+    QByteArrayData data[7];
+    char stringdata[108];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -33,11 +33,14 @@ QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
 QT_MOC_LITERAL(1, 11, 23), // "on_browseModule_clicked"
 QT_MOC_LITERAL(2, 35, 0), // ""
 QT_MOC_LITERAL(3, 36, 20), // "on_browseCSV_clicked"
-QT_MOC_LITERAL(4, 57, 25) // "on_execute_button_clicked"
+QT_MOC_LITERAL(4, 57, 25), // "on_execute_button_clicked"
+QT_MOC_LITERAL(5, 83, 12), // "check_params"
+QT_MOC_LITERAL(6, 96, 11) // "vector<int>"
 
     },
     "MainWindow\0on_browseModule_clicked\0\0"
-    "on_browseCSV_clicked\0on_execute_button_clicked"
+    "on_browseCSV_clicked\0on_execute_button_clicked\0"
+    "check_params\0vector<int>"
 };
 #undef QT_MOC_LITERAL
 
@@ -47,7 +50,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   14, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -55,14 +58,16 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   29,    2, 0x08 /* Private */,
-       3,    0,   30,    2, 0x08 /* Private */,
-       4,    0,   31,    2, 0x08 /* Private */,
+       1,    0,   34,    2, 0x08 /* Private */,
+       3,    0,   35,    2, 0x08 /* Private */,
+       4,    0,   36,    2, 0x08 /* Private */,
+       5,    0,   37,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Int,
+    0x80000000 | 6,
 
        0        // eod
 };
@@ -76,6 +81,8 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 1: _t->on_browseCSV_clicked(); break;
         case 2: { int _r = _t->on_execute_button_clicked();
             if (_a[0]) *reinterpret_cast< int*>(_a[0]) = _r; }  break;
+        case 3: { vector<int> _r = _t->check_params();
+            if (_a[0]) *reinterpret_cast< vector<int>*>(_a[0]) = _r; }  break;
         default: ;
         }
     }
@@ -106,13 +113,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 3)
+        if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
+        _id -= 4;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 3)
+        if (_id < 4)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 3;
+        _id -= 4;
     }
     return _id;
 }
