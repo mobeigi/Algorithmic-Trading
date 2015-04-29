474afd7d55a4af4af41c114d1e9d0936b273b03f
Mohammad Ghasembeigi <webmaster@mohammadg.com>
2015-04-29 20:18:46 +1000

    Update tester.py to be compatible with Wolf of Seng
    Also added --clean option to remove unneeded files after failed test.

--- tools/External Tester/tester.py
+++ tools/External Tester/tester.py
@@ -5,11 +5,15 @@
 # with an expected output over a serious of tests. It makes use of a random equity trading data
 # application to provide its input.
 #
-# usage: python tester.py trockAT TrockEquityDataGen
+# usage: tester.py [module application] [TrockEquityDataGen]
 #
 # or (if required applications trockAT and TrockEquityDataGen are in current script directory)
 #
-# usage: python teser.py
+# usage: teser.py
+#
+# Additional flags
+#
+# tester.py --clean   #cleans all test files in script directory
 #
 # Copyright (c) 2015 Trock. All rights reserved.
 
@@ -21,33 +25,48 @@ def main(argv):
 
   # Higher variables
   curDirectoryPath = os.path.dirname(os.path.abspath(__file__)) + "/";
-  trockATPath = "";
+  modulePath = "";
   trockDataGenPath = "";
   
-  #Auto detect required executables if in current directory
+  paramFilePath = curDirectoryPath + "test_ParamFile.param";
+  secondParamFilePath = curDirectoryPath + "test_ParamFile2.param";
+  testerOrderFile = curDirectoryPath + "test_orders_testerScript.csv"
+  trockOrderFile = curDirectoryPath + "orders.csv";
+  trockLogFile = curDirectoryPath + "test_logfile.txt";
+  genPathOutPut = curDirectoryPath + "test_genInput.csv";
+  wosLogFile = curDirectoryPath + "log.txt";
+  
+  #Auto detect required executables () if in current directory
   if os.path.isfile(curDirectoryPath + "trockAT.exe"):
-    trockATPath = curDirectoryPath + "trockAT.exe";
+    modulePath = curDirectoryPath + "trockAT.exe";
   elif os.path.isfile(curDirectoryPath + "trockAT"):
-    trockATPath = curDirectoryPath + "trockAT";
+    modulePath = curDirectoryPath + "trockAT";
 
   if os.path.isfile(curDirectoryPath + "TrockEquityDataGen.exe"):
     trockDataGenPath = curDirectoryPath + "TrockEquityDataGen.exe";
   elif os.path.isfile(curDirectoryPath + "TrockEquityDataGen"):
     trockDataGenPath = curDirectoryPath + "TrockEquityDataGen";
   
-  if (not trockATPath and not trockATPath):
+  #Add clean command to remove files
+  if len(sys.argv) == 2 and sys.argv[1] == "--clean":
+    removemaybe(trockOrderFile);
+    removemaybe(testerOrderFile);
+    removemaybe(trockLogFile);
+    removemaybe(genPathOutPut);
+    removemaybe(paramFilePath);
+    removemaybe(secondParamFilePath);
+    removemaybe(wosLogFile);
+    sys.exit(1);
+  
+  if (not modulePath and not modulePath):
     if len(sys.argv) != 3:
-      print 'usage:', sys.argv[0], '[trockAT] [TrockEquityDataGen]';
+      print 'usage:', sys.argv[0], '[module application] [TrockEquityDataGen]';
       sys.exit(1);
     else:
-      trockATPath = sys.argv[1];
+      modulePath = sys.argv[1];
       trockDataGenPath = sys.argv[2]; 
   
-  paramFilePath = curDirectoryPath + "test_ParamFile.param";
-  testerOrderFile = curDirectoryPath + "test_orders_testerScript.csv"
-  trockOrderFile = curDirectoryPath + "test_orders_trockAT.csv";
-  trockLogFile = curDirectoryPath + "test_logfile.txt";
-  
+
   testNumber = 30;
   
   for test in range(1, testNumber + 1):
@@ -66,28 +85,23 @@ def main(argv):
     genStartDay = genStartDate.day;
     genStartMonth = genStartDate.month;
     genStartYear = genStartDate.year;
-    genPathOutPut = curDirectoryPath + "test_genInput.csv";
 
     #Generate the test input for this round
     subprocess.call([trockDataGenPath, str(genNumTrades), str(genStartDay), str(genStartMonth), str(genStartYear), genPathOutPut])
 
     #Generate param file to use with TrockAT
-    start = datetime.datetime(genStartYear, genStartMonth, genStartDay);
+    startDate = datetime.datetime(genStartYear, genStartMonth, genStartDay);
     step = datetime.timedelta(days= genNumTrades - 1); #our end date is exclusive, no trade day generated
-    end = start + step;
-
-    # Generate proper date formats
-    startStr = start.strftime('%d-%b-%Y').upper();
-    endStr = end.strftime('%d-%b-%Y').upper();
+    endDate = startDate + step;
     
     #Generate returnsInCalculations and threshold
     genReturnsInCalc = random.randint(1, genNumTrades - 1);
-    genThreshold = random.uniform(0.0, 1.000);
+    genThreshold = random.uniform(0.0, 0.001);
+    
+    genParamFile(paramFilePath, secondParamFilePath, genPathOutPut, trockOrderFile, trockLogFile, genReturnsInCalc, genThreshold, startDate, endDate);
 
-    genParamFile(paramFilePath, genPathOutPut, trockOrderFile, trockLogFile, genReturnsInCalc, genThreshold, startStr, endStr);
-
-    # Call trockAT program to parse input
-    subprocess.call([trockATPath, paramFilePath])
+    # Call module application to parse input
+    subprocess.call([modulePath, genPathOutPut, secondParamFilePath, paramFilePath])
 
     #Call MSM analyser
     MSMAnalyser(genPathOutPut, testerOrderFile, genReturnsInCalc, genThreshold);
@@ -95,42 +109,54 @@ def main(argv):
     #Print current test num
     sys.stdout.write("Test #" + str(test) + ": ");
     
-    #Compare analyser output with trockAT output
-    diffNum = diffCSV(trockOrderFile, testerOrderFile);
-  
-    #Delete unneed files
-    os.remove(trockOrderFile);
-    os.remove(testerOrderFile);
-    os.remove(trockLogFile);
-    os.remove(genPathOutPut);
-    os.remove(paramFilePath);
+    #Compare analyser output with module application output
+    diffNum = diffOrderCSV(testerOrderFile, trockOrderFile);
   
     #Testing failed, terminate now
     if (diffNum > 0):
       print "FAILED!";
-      print "\t Found in files:", trockOrderFile, testerOrderFile, "\n", "Test number was " + str(test), ", testing", str(genNumTrades), "trade days.";
+      print "\t Found in files:", testerOrderFile, trockOrderFile, "\n", "Test number was " + str(test), ", testing", str(genNumTrades), "trade days.";
       sys.exit(1);
     else:
+      #Delete files from succesful test
+      removemaybe(trockOrderFile);
+      removemaybe(testerOrderFile);
+      removemaybe(trockLogFile);
+      removemaybe(genPathOutPut);
+      removemaybe(paramFilePath);
+      removemaybe(secondParamFilePath);
+      removemaybe(wosLogFile);
       print "PASSED";
     
     sleep(0.5); #small pause to allow applications to read/write files
     
-  #After all tests are passed print out message
+  #After all tests are passed print out results
   print "\nPassed all " + str(testNumber) + " tests successfully.";
   
 
 # Given required parameters, generated a valid paramfile
-def genParamFile(outputParamFilePath, inputCSVPath, outputCSVPath, outputLogFilePath, returnsInCalculation, threshold, startDate, endDate):
+def genParamFile(outputParamFilePath, outputSecondParamFilePath, inputCSVPath, outputCSVPath, outputLogFilePath, returnsInCalculation, threshold, startDate, endDate):
   f = open(outputParamFilePath, 'w');
+  
+  # Generate proper date formats
+  startDateStr = startDate.strftime('%d-%b-%Y').upper();
+  endDateStr = endDate.strftime('%d-%b-%Y').upper();
+  
   f.write(":input_csvFile:" + inputCSVPath.replace('\\', '/') + "\\\n" +
           ":output_csvFile:" + outputCSVPath.replace('\\', '/') + "\\\n" +
           ":output_logFile:" + outputLogFilePath.replace('\\', '/') + "\\\n" +
           ":returnsInCalculation:" + str(returnsInCalculation) + "\\\n" +
           ":threshold:" + str(threshold) + "\\\n" +
-          ":startDate:" + startDate + "\\\n" +
-          ":startDate:" + endDate + "\\\n"
+          ":startDate:" + startDateStr + "\\\n" +
+          ":startDate:" + endDateStr + "\\\n"
          );
   f.close();
+  
+  f2 = open(outputSecondParamFilePath, 'w');
+  f2.write("N,TH,DateRange\n" +
+          str(returnsInCalculation) + "," + str(threshold) + "," + str(startDate.year) + "-" + str(endDate.year) + "\n"
+          );
+  f2.close();
 
 # Given an input CSV file, applies MSM to generate the correct, expected output
 def MSMAnalyser(inputCSVFile, outputOrderFile, returnsInCalculation, threshhold):
@@ -218,17 +244,56 @@ def MSMAnalyser(inputCSVFile, outputOrderFile, returnsInCalculation, threshhold)
     
   f.close();
   
-#Compares two CSV files line by line  
-def diffCSV(first, second):
+#remove if exists
+def removemaybe(filename):
+  try:
+    os.remove(filename)
+  except OSError:
+    pass  
+
+#Compares two CSV files and compares critical values to ensure they are the same
+#First parameter should be tester generated CSV file
+def diffOrderCSV(first, second):
   f1 = open(first, 'rb')
   f2 = open(second, 'rb')
   reader1 = csv.reader(f1)
   reader2 = csv.reader(f2)
-
-  differencesFound = 0;
   
+  differencesFound = 0;
+  lineNum = 0;
+  
+  # Uses itertools to get array components for each line
   for lhs, rhs in itertools.izip(reader1, reader2):
-    if lhs != rhs:
+    lineNum += 1;
+
+    #Ignore header
+    if (lineNum == 1):
+      continue;
+      
+    #Compare critical components
+    diff = False;
+    
+    #company name
+    if lhs[0] != rhs[0]:
+      diff = True;
+      
+    #Check dates  
+    if lhs[1] != rhs[1]:
+      diff = True;
+    
+    #Check prices  
+    lSignal = lhs[5];
+    rSignal = rhs[5];
+    
+    if rSignal == "Buy":
+      rSignal = "B";
+    elif rSignal == "Sell":
+      rSignal = "S"; 
+    
+    if lSignal != rSignal:
+      diff = True;
+
+    if diff:
       differencesFound += 1;
       print "diff #" + str(differencesFound) + ": ", lhs, rhs
       
