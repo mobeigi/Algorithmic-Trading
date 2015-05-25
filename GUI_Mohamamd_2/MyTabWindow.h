90ad4287af11b1ff54343a938ce1a653d7dbc296
Mohammad Ghasembeigi <webmaster@mohammadg.com>
2015-05-26 04:05:08 +1000

    Add GUI with summary

---
+++ GUI_Mohamamd_2/MyTabWindow.h
@@ -0,0 +1,51 @@
+//myTabWidget.h File
+
+#ifndef MYTABWIDGET_H
+#define MYTABWIDGET_H
+ 
+#include <QTabWidget> 
+#include <QMessageBox>
+#include <QString>
+#include "ParseCSVData.h"
+#include "DisplayAnalysis.h"
+#include <set>
+
+class MyTabWidget: public QTabWidget
+{
+  Q_OBJECT
+  public:    
+    MyTabWidget(QWidget* parent, std::ParseCSVData *pCSVdata)
+	{
+      this->setParent(parent);
+      this->pCSVdata = pCSVdata;
+      connect(this , SIGNAL(currentChanged(int)),this,SLOT(currentChangedSlot(int)));
+	};
+
+    void addEqType(std::string &eqType) { eqTypeVec.push_back(eqType); }
+
+	~ MyTabWidget(){};	
+
+  private:
+    std::ParseCSVData *pCSVdata;
+    std::vector<std::string> eqTypeVec;
+    std::set<int> isCached;
+
+  public slots:
+	//Slot that is called when Tab selection changes
+	void currentChangedSlot(int index)
+    {
+        //Don't process summary
+        if (index == 0) {
+            return;
+        }
+
+        //If the tab has not already been loaded then load it
+        if (isCached.find(index) == isCached.end()) {
+            DisplayAnalysis *dw = qobject_cast<DisplayAnalysis *>(this->widget(index));
+            dw->displayAnalysis(this->pCSVdata->getDataForEquityType(this->eqTypeVec[index]));
+            isCached.insert(index);
+        }
+	};
+  
+};
+#endif
